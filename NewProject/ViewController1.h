4e8c7c2312c4b083a7876e4a0f4525b33341f7a6
Bala Krishna <balakrishna@balas-macbook-pro.local>
2020-02-04 22:22:36 +0530

    add map view

---
+++ NewProject/ViewController1.h
@@ -0,0 +1,13 @@
+//
+//  ViewController1.h
+//  NewProject
+//
+//  Created by Bala Krishna on 04/02/20.
+//  Copyright © 2020 Bala Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController1 : UIViewController
+
+@end
---
+++ NewProject/ViewController1.m
@@ -0,0 +1,42 @@
+//
+//  ViewController1.m
+//  NewProject
+//
+//  Created by Bala Krishna on 04/02/20.
+//  Copyright © 2020 Bala Krishna. All rights reserved.
+//
+
+#import "ViewController1.h"
+#import <MapKit/MapKit.h>
+
+@interface ViewController1 ()
+
+@end
+
+@implementation ViewController1
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    MKMapView*map = [[MKMapView alloc]init];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
