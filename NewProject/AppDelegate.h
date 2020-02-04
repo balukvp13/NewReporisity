72de0d0adba1c04c1620ee07f64c9599cdb2fdb3
Bala Krishna <balakrishna@balas-macbook-pro.local>
2020-02-04 21:50:29 +0530

    Initial Commit

---
+++ NewProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewProject
+//
+//  Created by Bala Krishna on 04/02/20.
+//  Copyright © 2020 Bala Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewProject
+//
+//  Created by Bala Krishna on 04/02/20.
+//  Copyright © 2020 Bala Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
